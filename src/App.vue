c72140495794e586ebc7a8e4e76873991e46a190
armm-wayakna <armm@wayakna.com>
2023-02-15 15:51:04 -0600

    Creación del proyecto y Splashscreen

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,46 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+    <!-- Suspense es un componente que recibe dos slots: default y fallback -->
+    <Suspense>
+        <template #default>
+            <!-- Componente que queremos mostrar cuando async termine de cargar -->
+            <HomePage />
+        </template>
+        <template #fallback>
+            <SplashScreen /> <!-- Componente que queremos mostrar mientras async carga -->
+        </template>
+    </Suspense>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+    import SplashScreen from "./components/SplashScreen.vue";
+    import { defineAsyncComponent } from "vue"; // defineAsyncComponent es una función que nos permite cargar componentes de forma asíncrona.
 
-export default {
-  name: "App",
-  components: {
-    HelloWorld,
-  },
-};
+    export default {
+        name: "App",
+        components: {
+            SplashScreen,
+            HomePage: defineAsyncComponent(() => {
+                return new Promise((resolve) => {
+                    setTimeout(() => {
+                        resolve(import("./components/HomePage.vue"));
+                    }, 3000);
+                });
+            }), // defineAsyncComponent recibe una función que retorna una promesa que resuelve el componente que queremos cargar.
+        },
+    };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+html,
+body,
+.app {
+  min-height: 100vh;
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+* {
+  --brand-green: #04b500;
+  --brand-blue: #0689b0;
 }
 </style>

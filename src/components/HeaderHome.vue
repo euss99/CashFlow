b940192f53aacd6d2be03e43cc34a8752c04aaa8
armm-wayakna <armm@wayakna.com>
2023-02-16 13:08:57 -0600

    [ADD] Header y contenido

---
+++ src/components/HeaderHome.vue
@@ -0,0 +1,41 @@
+<template>
+    <header>
+        <img src="@/assets/cashflow-icon.svg" alt="Logo" @click="reload">
+        <h1><span>Cash</span>Flow</h1>
+        <span></span>
+    </header>
+</template>
+
+<script>
+    export default {
+        name: 'HeaderHome',
+        methods: {
+            reload() {
+                window.location.reload();
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    header {
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+        width: 100vw;
+    }
+    img {
+        cursor: pointer;
+    }
+    img,
+    span {
+        width: 48px;
+    }
+    h1 {
+        font-size: 1.4rem;
+        color: var(--brand-blue);
+    }
+    h1 span {
+        color: var(--brand-green);
+    }
+</style>
--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,3 +1,21 @@
 <template>
-  <h1>Home</h1>
+  <layout-home>
+    <template #header>
+      <header-home />
+    </template>
+  </layout-home>
 </template>
+
+<script>
+  import LayoutHome from '@/components/LayoutHome.vue';
+  import HeaderHome from '@/components/HeaderHome.vue';
+
+  export default {
+    name: 'HomePage',
+    components: {
+      LayoutHome,
+      HeaderHome,
+    },
+  };
+
+</script>

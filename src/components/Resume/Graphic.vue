eb9483582a57cc078cdf15b6a6bd3b2810146899
armm-wayakna <armm@wayakna.com>
2023-02-20 13:50:40 -0600

    [ADD] Creación de la lógica del gráfico

--- src/components/Resume/Graphic.vue
+++ src/components/Resume/Graphic.vue
@@ -5,7 +5,7 @@
             <line 
                 stroke="#c4c4c4"
                 stroke-width="2"
-                x1="0" y1="100" x2="300" y2="100"
+                x1="0" :y1="zero" x2="300" :y2="zero"
             />
             <!-- Línea costos -->
             <polyline 
@@ -38,24 +38,31 @@
     // Haciendo que la propiedad amounts sea reactiva.
     const { amounts } = toRefs(props);
 
+    /* ==== Función amountToPixels convierte el monto en pixele ==== */
     const amountToPixels = (amount) => {
         const minX = Math.min(...amounts.value); // Valor mínimo del array.
         const maxX = Math.max(...amounts.value); // Valor máximo del array.
+        const amountAbs = amount + Math.abs(minX); // Valor absoluto de la cantidad.
+        const minmax = Math.abs(minX) + Math.abs(maxX); // Valor absoluto de la diferencia entre el mínimo y el máximo.        
 
-        return `${minX} ${maxX}`;
+        return 200 - ((amountAbs * 100) / minmax * 2); // Retornando la cantidad de puntos en el eje y para la línea de costos.
     }
 
+    const zero = computed(() => {
+        return amountToPixels(0);
+    }); // Función que retorna la cantidad del eje y donde se encuentra la línea central en cero.
+
+    /* ==== Función de unión de puntos ==== */
     const points = computed(() => {
         const total = amounts.value.length; // Cantidad de elementos en el array.
 
         // Retornando los puntos para la línea.
-        return Array(total).fill(100).reduce((points, amount, index)=> {
+        return amounts.value.reduce((points, amount, index)=> {
             // points = puntos anteriores.
             // amount = cantidad de puntos en el eje y.
             // index = índice del elemento en el array.
             const x = (300 / total) * (index + 1); // Cantidad de puntos en el eje x.
             const y = amountToPixels(amount); // Cantidad de puntos en el eje y.
-            console.log(y);
             return `${points} ${x} ${y}`; // Concatenando los puntos.
         }, "0,100");
     });
